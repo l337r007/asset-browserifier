c627a5adef1a039a5169a342b9c9233ef33b22f4
Wolfgang Herget <wolfgang.herget@dfki.de>
2015-05-19 17:52:44 +0200

    add main AssetBrowserifier module

---
+++ index.js
@@ -0,0 +1,69 @@
+var through = require('through2');
+var gutil = require('gulp-util');
+var File = require('vinyl');
+var stockBrowserify = require('browserify');
+var multimatch = require('multimatch');
+
+function everyFileFunction(instance, outputFile) {
+	return function (file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new gutil.PluginError('gulp-asset-broserify', 'Streaming not supported'));
+			return;
+		}
+
+		if (multimatch(file.path, instance._filter)) {
+			if (instance._files[outputFile] === undefined) {
+				instance._files[outputFile] = [];
+			}
+			instance._files[outputFile].push(file);
+		}
+
+		cb();
+	};
+};
+
+function flushFunction(instance, outputFile) {
+	return function (doneFlush) {
+		var out = this;
+		var myFiles = instance._files[outputFile];
+		if (myFiles.length > 0) {
+			instance._browserify(myFiles, {basedir: myFiles[0].base})
+					.bundle(function (err, result) {
+						if (err) {
+							doneFlush(err);
+						} else {
+							// push result into out
+							var f = new File({
+								path: outputFile,
+								contents: result
+							});
+							out.push(f);
+							delete myFiles;
+							doneFlush();
+						}
+					});
+		} else {
+			doneFlush();
+		}
+	};
+};
+
+var AssetBrowserifier = function (browserify, nameFilter) {
+	this._filter = nameFilter || ["*.js"];
+	this._browserify = browserify || stockBrowserify;
+	this._files = {};
+	var instance = this;
+	this.stream = function (outputFile) {
+		return through.obj(
+			everyFileFunction(instance, outputFile),
+			flushFunction(instance, outputFile)
+			);
+		};
+	};
+
+module.exports = AssetBrowserifier;
